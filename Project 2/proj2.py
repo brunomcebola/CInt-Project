d71b8453ef453739f5a241a786127fcd00e9ad82
Bruno Cebola <bruno.m.cebola@gmail.com>
2022-10-27 11:39:18 +0100

    backup before tests

--- Project 2/proj2.py
+++ Project 2/proj2.py
@@ -2,6 +2,7 @@ from cmath import inf
 from math import floor
 import random
 from tkinter import Place
+import pandas as pd
 
 from deap import base
 from deap import creator
@@ -27,46 +28,60 @@ def getPlaceList(nb_places: int):
     return options
 
 
-def evalRoute(individual):
+def getRoute(individual, order_matrix):
+    aux_route = []
+    full_route = []
 
-    list_of_routes = []
-    route = []
-    capacity = 0
-
-    flag = False
+    current_load = 0
 
     for place in individual:
 
-        distance_of_route = individual #TODO here comes the data from the matrix | NOT WHAT IT IS NOW
+        order = int(order_matrix.loc[order_matrix["Customer"] == place]['Orders'])
+
+        current_load += order
         
-        capacity += distance_of_route
-
-        if capacity <= 1000:
-            route.append(place)
+        if current_load <= MAX_LOAD:
+            aux_route.append(place)
         else:
-            list_of_routes.append(route)
-            route.clear()
-            route.append(place)
-            capacity = distance_of_route
+            full_route.append(aux_route)
+            aux_route = []
+            aux_route.append(place)
+            current_load = 0
 
-    # Calculate the distances made
+    full_route.append(aux_route)
+    aux_route = [0]
+
+    for route in full_route:
+        aux_route += route + [0]
+
+    return aux_route
+
+
+def evalRoute(individual, order_matrix, dist_matrix):
+
+    route = getRoute(individual, order_matrix)
+
+    # Calculate the total distance
     total_distance = 0
 
-    for route in list_of_routes:
-        for index, place in enumerate(route):
-            if index == 0:
-                distance = individual #TODO distance from warehouse to place
-                total_distance += distance
-            elif index == len(route) + 1:
-                distance = individual #TODO distance from place to warehouse
-                total_distance += distance
-            else:
-                distance = individual #TODO distance from place before to here
-                total_distance += distance
-                
+    for prev_place, current_place in zip(route[:-1], route[1:]):
+        total_distance += dist_matrix[prev_place][current_place]
+
     return (total_distance,)
 
 
+def readCSV(file_name):
+    return pd.read_csv(file_name)
+
+
+# READ DATA FROM CSV
+dist_matrix = readCSV("CustDist_WHCentral.csv").iloc[:, 1:]
+dist_matrix.columns = dist_matrix.columns.astype(int)
+
+order_matrix = readCSV("CustOrd.csv")
+
+
+# CREATE EA
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)  # type: ignore
 
@@ -78,7 +93,7 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.pl
 
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)  # type: ignore
 
-toolbox.register("evaluate", evalRoute)
+toolbox.register("evaluate", evalRoute, order_matrix=order_matrix, dist_matrix=dist_matrix)
 
 toolbox.register("crossover", tools.cxOrdered)
 
